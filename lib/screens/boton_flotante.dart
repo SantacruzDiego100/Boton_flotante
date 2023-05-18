4e3509f967e77e57f6d6f5284237873a83ac10db
DIEGO_LEONARDO_SANTACRUZ_CASTILLO <a.20308051281033@cbtis128.edu.mx>
2023-05-18 21:38:03 +0000

    Boton_flotante

---
+++ lib/screens/boton_flotante.dart
@@ -0,0 +1,43 @@
+//Importacion de libreria material
+import 'package:flutter/material.dart';
+
+//Crea la clase y el estado a cambiar
+class BotonFlotante extends StatefulWidget {
+  const BotonFlotante({super.key});
+  _BotonFlotante createState() => _BotonFlotante();
+}
+
+//Crear los widgets a mostrar
+class _BotonFlotante extends State<BotonFlotante> {
+  String txt="";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Boton Flotante'),
+      ),
+      body: Center(
+        child: Column(  
+            mainAxisAlignment: MainAxisAlignment.center,  
+            children: [
+              Text(txt)
+             ], 
+          )
+      ),
+      floatingActionButton: Row(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+         children: [
+            FloatingActionButton(
+                      child: Icon(Icons.visibility),
+                     onPressed: () => setState(() => txt="Hola mundo")
+                       ),
+                      
+                    FloatingActionButton(
+                              child: Icon(Icons.visibility_off),
+                              onPressed: () => setState(() => txt="")
+                            ),
+          ], 
+       ),
+    );
+  }
+}
